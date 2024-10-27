e01ffaaacbebc11cb4c61aeafc94cc0aa7b14304
supreeya10 <supreeyaadhikari480@gmail.com>
2024-10-27 23:48:38 +0545

    Toolbox Page is Modified Using Component

--- src/Toolbox/Toolbox.jsx
+++ src/Toolbox/Toolbox.jsx
@@ -1,5 +1,6 @@
 import styles from "./Toolbox.module.css";
 import { useNavigate } from "react-router-dom";
+import ToolboxContainer from "./ToolboxContainer";
 
 export default function Toolbox() {
   const navigate = useNavigate();
@@ -9,31 +10,19 @@ export default function Toolbox() {
       <h1 className={styles.toolboxTitle}>Toolbox</h1>
 
       <div className={styles.toolsSection}>
-        <div className={styles.toolCard}>
-          <h2 className={styles.toolTitle}>Calculator</h2>
-          <p className={styles.toolDescription}>
-            A handy calculator to perform basic arithmetic operations.
-          </p>
-          <button
-            className={styles.toolButton}
-            onClick={() => navigate("/calculator")}
-          >
-            Open Calculator
-          </button>
-        </div>
+        <ToolboxContainer
+          title="Calculator"
+          desc="A handy calculator to perform basic arithmetic operations."
+          btnName="Open Calculator"
+          clickBtn={() => navigate("/calculator")} 
+        />
 
-        <div className={styles.toolCard}>
-          <h2 className={styles.toolTitle}>Currency Converter</h2>
-          <p className={styles.toolDescription}>
-            Quickly convert between various currencies in real-time.
-          </p>
-          <button
-            className={styles.toolButton}
-            onClick={() => navigate("/currencyconverter")}
-          >
-            Open Currency Converter
-          </button>
-        </div>
+        <ToolboxContainer
+          title="Currency Converter"
+          desc="Quickly convert between various currencies in real-time."
+          btnName="Open Currency Converter"
+          clickBtn={() => navigate("/currencyconverter")}
+        />
       </div>
     </div>
   );
